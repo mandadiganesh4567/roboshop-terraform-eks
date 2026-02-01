4c949cb580d3d371eddba0b431a4254e01fd6f2a
Ganesh Mandadi <mandadiganesh4567@gmail.com>
2026-02-01 22:58:58 +0530

    roboshop-eks

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -8,12 +8,12 @@ terraform {
 
   backend "s3" {
     bucket = "ganesh-lab-demo"
-    key    = "roboshop-dev-sg"
+    key    = "roboshop-dev-vpc"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
   }
-}
+} 
 
 provider "aws" {
   region = "us-east-1"
